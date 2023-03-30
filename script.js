0ba4ce25298e85fcb5ce6764a58c94054d49f518
estif2127 <35039738+estif2127@users.noreply.github.com>
2023-03-29 20:37:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ const quotes = [
     author: "Abraham Lincoln"
   },
   {
+    quote: "A good name is better than a good perfume.",
+    author: "Unknown"
+  },
+    {
+    quote: "When the Nile River knows a secret the desert will soon come to know it too.",
+    author: "Unknown"
+  },
+    {
+    quote: "Hardships do not come to those that are prepared.",
+    author: "Unknown"
+  },
+      {
+    quote: " A home cannot be built for the rainy season that has past.",
+    author: "Unknown"
+  },
+  {
     quote: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs"
   },
   {
     quote: "If you want to achieve greatness, stop asking for permission.",
@@ -14,6 +29,10 @@ const quotes = [
   {
     quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
     author: "Albert Schweitzer"
+  },
+    {
+    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    author: "Steve Jobs"
   },
   {
     quote: "Believe you can and you're halfway there.",
